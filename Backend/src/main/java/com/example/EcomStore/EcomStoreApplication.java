4f7321b02d2f8c439c44a9faab964deaf9ef8bca
Eman Bilal <fa22-bcs-088@cuilahore.edu.pk>
2026-07-11 12:20:49 +0500

    Initial commit: Set up Spring Boot backend structure and monorepo root

---
+++ Backend/src/main/java/com/example/EcomStore/EcomStoreApplication.java
@@ -0,0 +1,15 @@
+package com.example.EcomStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomStoreApplication.class, args);
+
+		System.out.println("ngdjhbkfdhbkhbfv");
+	}
+
+}
