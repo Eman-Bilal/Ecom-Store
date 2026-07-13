435f36d5748a5aff717b1f4f299d0eb190d5c3c1
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-13 17:20:32 +0500

    Repositories added

---
+++ API/src/main/java/com/example/EcomStore/Repository/CustomerOrderRepository.java
@@ -0,0 +1,11 @@
+package com.example.EcomStore.Repository;
+
+import com.example.EcomStore.Entities.CustomerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+
+public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, String> {
+  long countByPlacedAtBetween(LocalDateTime start, LocalDateTime end);
+
+}
--- API/src/main/java/com/example/EcomStore/Repository/UserRepository.java
+++ API/src/main/java/com/example/EcomStore/Repository/UserRepository.java
@@ -2,13 +2,10 @@ package com.example.EcomStore.Repository;
 
 import com.example.EcomStore.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
-  Optional<User> findByUserDetail_Email(String email);
+  Optional<User> findByEmail(String email);
 }
