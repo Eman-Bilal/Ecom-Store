147905d7d0091d0c5efc250c6f3f85826360a4cc
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-13 17:21:12 +0500

    Services added

--- API/src/main/java/com/example/EcomStore/Service/UserService.java
+++ API/src/main/java/com/example/EcomStore/Service/UserService.java
@@ -1,5 +1,29 @@
 package com.example.EcomStore.Service;
 
+import com.example.EcomStore.Entities.User;
+import com.example.EcomStore.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserService {
+  UserRepository user;
+
+  public List<User> getUser() {
+    return user.findAll();
+  }
+
+  public ResponseEntity<String> createUser(){
+
+    return ResponseEntity.ok("New user added");
+  }
 
 }
