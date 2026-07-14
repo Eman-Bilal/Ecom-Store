1836d70d9c2ef3464ab1852aa40cb4bd578e80f9
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-14 15:22:25 +0500

    CategoryController added

---
+++ API/src/main/java/com/example/EcomStore/Controller/CategoryController.java
@@ -0,0 +1,49 @@
+package com.example.EcomStore.Controller;
+
+import com.example.EcomStore.Entities.Category;
+import com.example.EcomStore.Service.CategoryService;
+import jakarta.validation.Valid;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/categories/")
+public class CategoryController {
+  private final CategoryService categoryService;
+
+  @PostMapping("/create")
+  public ResponseEntity<Category> create(@Valid @RequestBody  Category category){
+    Category saved= categoryService.createCategory(category);
+    return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+  }
+
+  @GetMapping("/getAll")
+  public ResponseEntity<List<Category>> getAllCategories(){
+    return ResponseEntity.ok(categoryService.getAll());
+  }
+
+  @PutMapping("/update/{id}")
+  public void update(@PathVariable long id,@Valid @RequestBody Category category){
+    categoryService.updateCategory(id, category);
+  }
+  @GetMapping("/{id}")
+  public ResponseEntity<Category> getCategoryById(@PathVariable long id){
+    return ResponseEntity.ok(categoryService.getById(id));
+  }
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<String> deleteCategory(@PathVariable long id){
+    categoryService.deleteCategory(id);
+    return ResponseEntity.ok("Category safely inactivated");
+  }
+
+  @PatchMapping("/reactivate/{id}")
+  public ResponseEntity<Category> reactivate(@PathVariable Long id) {
+    return ResponseEntity.ok(categoryService.reactivateCategory(id));
+  }
+}
+
