cb75cdb2a69b1916140adebf143a1311b5732259
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-27 10:11:32 +0500

    Updated

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { App } from './app/app';
-import routeConfig from './app/routes';
+import routeConfig from './app/app.routes';
 
 
 bootstrapApplication(App, {
